428d4e7d9ce03598e03b0f2b32f0ed9a175fe897
Carol <carolchristovamc@gmail.com>
2023-06-28 20:34:37 -0300

    feat: creating brownie activity

---
+++ app/src/main/java/com/example/receita/recipes/DarkBrownieActivity.kt
@@ -0,0 +1,22 @@
+package com.example.receita.recipes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.receita.databinding.ActivityBrownieBinding
+import com.example.receita.databinding.ActivityCapuccinoBinding
+import com.example.receita.databinding.ActivityMainBinding
+
+class DarkBrownieActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityBrownieBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBrownieBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+    }
+
+
+
+}
